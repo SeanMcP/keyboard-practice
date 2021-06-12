299cf896d1c5dd7105e439a4c924233f3642d23b
Sean McPherson <git@seanmcp.com>
2021-06-12 09:51:39 -0400

    feat: add words mode

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 (async function main() {
   // STATE
   const state = {
+    list: [],
+    mode: "",
     name: "",
     reward: "🍓",
     rounds: 8,
@@ -12,19 +14,31 @@
     terminal.textContent = "";
   }
 
-  function echo(message) {
+  function echo(message, dangerouslyUseInnerHTML = false) {
     const line = document.createElement("div");
     line.classList.add("line");
-    line.textContent = message;
+    if (dangerouslyUseInnerHTML) {
+      line.innerHTML = message;
+    } else {
+      line.textContent = message;
+    }
     terminal.appendChild(line);
   }
 
-  function generateCharacter() {
-    let character = Math.random().toString(36).substring(2, 3);
-    if (Math.random() > 0.5) {
-      character = character.toUpperCase();
+  function generateExpected() {
+    if (state.mode === "C") {
+      let character = Math.random().toString(36).substring(2, 3);
+      if (Math.random() > 0.5) {
+        character = character.toUpperCase();
+      }
+      return character;
+    } else if (state.mode === "W") {
+      let word = state.list[state.score];
+      if (word === "god" || Math.random() > 0.5) {
+        word = word[0].toUpperCase() + word.slice(1);
+      }
+      return word;
     }
-    return character;
   }
 
   function handleClick(event) {
@@ -48,6 +62,7 @@
   async function read(required = false) {
     return new Promise((resolve) => {
       const input = document.createElement("input");
+      input.autocomplete = "off";
       input.id = "input";
 
       input.addEventListener("keypress", (event) => {
@@ -81,6 +96,16 @@
   const name = await read(true);
   state.name = name;
 
+  echo("Practices characters (C) or words (W)?");
+  while (!state.mode) {
+    const mode = await read(true);
+    if (mode === "C" || mode === "W") {
+      state.mode = mode;
+    } else {
+      echo("😅 Oops! Please enter C or W:");
+    }
+  }
+
   echo("What reward are you working towards? (optional)");
   const reward = await read();
   if (reward) state.reward = reward;
@@ -88,22 +113,31 @@
   echo(`How many times do you want to practice? (default: ${state.rounds})`);
   const rounds = await read();
   if (rounds) state.rounds = rounds;
+  if (state.mode === "W") {
+    const { default: cvcWords } = await import("./cvc-words.js");
+    // h/t https://stackoverflow.com/a/46545530/8486161
+    state.list = cvcWords
+      .map((a) => ({ sort: Math.random(), value: a }))
+      .sort((a, b) => a.sort - b.sort)
+      .map((a) => a.value)
+      .slice(0, state.rounds);
+  }
 
   clear();
   echo(`Alright, ${state.name}! Let's practice typing!`);
   await sleep(3000);
 
   while (state.score < state.rounds) {
-    const character = generateCharacter();
+    const expected = generateExpected();
     let answer = "";
 
-    while (answer !== character) {
+    while (answer !== expected) {
       clear();
       echo("Score: " + printScore());
-      echo("Type:  " + character);
+      echo("Type:&nbsp;&nbsp;<b>" + expected + "</b>", true);
       answer = await read(true);
 
-      if (answer !== character) {
+      if (answer !== expected) {
         echo("🤔 Try again!");
         await sleep(2000);
       }
